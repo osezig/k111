97a8526b04eac746a177cfce8b644599d049494d
sergey_iagfarov <dedaz27@mail.ru>
2022-10-14 21:13:29 +0500

    Задания 3 и 4

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,39 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        task1and2();
+        task3();
+        task4();
     }
 
-    public static void task1and2() {
+    public static void task3() {
         int[] weights = new int[3];
         weights[0] = 1;
         weights[1] = 2;
         weights[2] = 3;
+        System.out.print(weights[2] + ", " + weights[1] + ", " + weights[0]);
+        System.out.println();
 
         double[] height = {1.57, 7.654, 9.986};
+        System.out.print(height[2] + ", " + height[1] + ", " + height[0]);
+        System.out.println();
 
         int[] thickness = new int[]{1, 2, 3, 4};
+        System.out.print(thickness[3] + ", " + thickness[2] + ", " + thickness[1] + ", " + thickness[0]);
+        System.out.println();
 
+    }
+
+    public static void task4() {
+        int[] weights = new int[3];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
         for (int i = 0; i < weights.length; i++) {
-            System.out.print(weights[i]);
-            if (i != weights.length - 1); {
-                System.out.print(", ");
+            if (weights[i] % 2 != 0){
+                weights[i] += 1;
             }
         }
-        System.out.println();
-
-        for (int i = 0; i < height.length; i++) {
-            System.out.print(height[i]);
-            if (i != height.length - 1);{
-                System.out.print(", ");}
-        }
-        System.out.println();
-
-        for (int i = 0; i < thickness.length; i++) {
-        System.out.print(thickness[i]);
-        if (i != thickness.length - 1); {
-            System.out.print(", ");
-        }
+        System.out.println(Arrays.toString(weights));
     }
-        System.out.println();
-        }
-        }
-
+}
